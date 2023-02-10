21ed9283410f41d706edc3ae80756e450debb487
NanGyung <rhrnakaktdidrod0926@gmail.com>
2023-02-10 16:38:36 +0900

    20230210

---
+++ 4.함수/고차함수/exam2.js
@@ -0,0 +1,35 @@
+{
+    {
+        // 배열을 입력받아 2배수 한 배열을 반환하는 함수를 구현하시오
+        const arr = [1,2,3];
+        function x(arr){
+            const array = arr.slice();
+            for(let i = 0; i < array.length;i ++){
+                array[i] *= 2;
+            }
+            return array;
+        }
+        console.log(x(arr));   
+    }
+
+    {
+        // 배열을 입력받아 3배수 한 배열을 반환하는 함수를 구현하시오
+        function x(arr){
+            const array = arr.slice();
+            for(let i = 0; i < array.length;i ++){
+                array[i] *= 3;
+            }
+            return array;
+        }
+        console.log(x(arr));   
+    }
+}
+
+{
+    const arr = [1,2,3];
+    function x(arr){
+        for(let i = 0; i < arr.length;i ++){
+            array[i] *= 3;
+            }
+        }
+}
