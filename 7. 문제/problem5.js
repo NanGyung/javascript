97cc63405ad497f84152a8f99e2fe029f73ed084
NanGyung <rhrnakaktdidrod0926@gmail.com>
2023-02-16 11:18:42 +0900

    23.02.16 quize

--- 7. 문제/problem5.js
+++ 7. 문제/problem5.js
@@ -1,115 +1,105 @@
+const data = `{
+    "members":[
+      {
+        "id":1,
+        "name":"홍길동1",
+        "age":10,
+        "gender":"남",
+        "hobby" : ["수영","등산","골프"]
+      },
+      {
+        "id":2,
+        "name":"홍길동2",
+        "age":20,
+        "gender":"여",
+        "hobby" : ["독서","수영"]
+      },
+      {
+        "id":3,
+        "name":"홍길동3",
+        "age":30,
+        "gender":"남",
+        "hobby" : ["영화","축구","게임"]
+      },
+      {
+        "id":4,
+        "name":"홍길동4",
+        "age":40,
+        "gender":"여",
+        "hobby" : ["수영","등산"]
+      },
+      {
+        "id":5,
+        "name":"홍길동5",
+        "age":50,
+        "gender":"여",
+        "hobby" : ["음악","영화","골프"]
+      }
+    ]
+  }`;
+
+// 공공API를 통해 위의 JSON포멧의 문자열을 받았다고 가정하고 아래문항에 대해 각각 코드로 구현하시오
+const arr = JSON.parse(data);
+// console.log(arr['members'][0]['hobby'][1]);
+
+// 1. 회원수는?
 {
-    // 문제1) 정수를 요소로 갖는 배열을 매개값으로 받아 합과 평균을 반환하는 함수를 구현하시오
-    // const number1 = v1 => {
-    //     let n1 = 0;
-    //     let sum = 0;
-    //     for(let i = 0; i < v1.length; i++){
-    //        n1 = v1[i];
-    //        sum += n1;
-    //     }
-    //     return sum;
-    // }
-
-    // function number2(v1,v2){
-    //     let result = 0;
-    //     result = v2 / v1.length + 1;
-        
-    //     return result;
-    // }   
-    // const arr = [1,2,3];
-    // const f1 = number1(arr);
-    // console.log(`합: ${f1}`);
-    // console.log(`평균: ${number2(arr,f1)}`);
-
-    function number(v1){
-        let n1 = 0;
-        let sum = 0;
-        let div = 0;
-        const result = [];
-        for(let i = 0; i < v1.length; i++){
-           n1 = v1[i];
-           sum += n1;
-           div = sum / v1.length + 1;
+    let cnt = arr['members'].length;
+    console.log(`회원수: ${cnt}명`);
+}
+// 2. 여성회원수는?
+{
+    function womanCnt(v1){
+        let count = 0;
+        for(let i = 0; i < v1['members'].length; i++){
+            const gender = v1['members'][i]['gender'];
+            if(gender == '여') count++;
         }
-        result[0] = '합: ' + sum;
-        result[1] = ' 평균: ' + div;
+        return count;
+    }
+    const cnt = womanCnt(arr);
+    console.log(`여성회원수: ${cnt}명`);
+}
+// 3. 남자회원 나이의 총합과 여자회원 나이의 총합?
+{
+    function ages(v1, v2){
+        const arr = v1['members'].filter(item => item['gender'] == v2);
+        let agetotal = 0;
+        for(let obj of arr){
+            agetotal += obj['age'];
+        }
+        return agetotal;
+    }
+
+    const woman = ages(arr,'여');
+    const man = ages(arr,'남');
+
+    console.log(`여성회원 나이 총합: ${woman}`);
+    console.log(`남성회원 나이 총합: ${man}`);
+
+}
+// 4. 골프가 취미인 회원의 이름을 나열하시오
+{
+    function hobbyName(v1){
+        const arr = v1.filter(item => item['hobby'] == '골프 ');
+        const result =  [];
+        for(let i = 0; i < v1.length; i++){
+            for(const ele of v1[i].hobby){
+                if(ele == '골프'){
+                    result[i] = v1[i].name;
+                }
+            }
+        }
         return result;
     }
-    const arr = [3,4,5];
-    console.log(`${number(arr)}`);
-    
-    // 문제2) 구구단 단수를 매개값으로 받아 구구단을 출력하는 함수를 구현하시오
-    function mutiple(v1){
-        const arr = [];
-        let result = 0;
-        let mutiply = 0;
-        for(let i = 0; i < 9; i++){
-          mutiply = v1 * (i+1);
-          result = v1 + '*' + (i+1) + '=' + mutiply;
-          arr[i] = result;  
-        }
-        return arr;
-    }
-    console.log(`구구단: ${mutiple(2)}`);
-    console.log(`구구단: ${mutiple(3)}`);
+        const names = hobbyName(members);
+        console.log(names);
+}
+// 5. 회원의 취미를 모두 나열하시오.
+{
 
-    // 문제3) 아래 해당되는 문자열을 매개값으로 받아 위도(lat)와 경도(lng)를 출력하는 함수를 구현하시오
-    // '[{"lat":34.123, "lng":127.123}, {"lat":35.123, "lng":132.345}, {"lat":36.123, "lng":136.456}]'
-    // const arr1 = [{lat:34.123, lng:127.123}, {lat:35.123, lng:132.345}, {lat:36.123, lng:136.456}];
-    // function find(v1){
-    //     let result = [];
-    //     for(let i = 0; i < v1.length; i++){
-    //      for(let ele of v1){
-    //         ele = [v1[i].lat, v1[i].lng];
-    //         result[i] = ele; 
-    //      }
-    // } 
-        
-    //    return result;
-    // }
-    // console.log(find(arr1));
+}
+// 6. 여성회원중 나이가 가장 많은 회원의 이름은?
+{
 
-    {   //  case1)
-        function latLng(json){
-            // 1) json포맷 문자열 => js로 변환
-            const json2 = JSON.parse(json);
-            let result = [];
-            // 2) 새로운 배열에 아래 요소 저장
-            // 위도 : xxx , 경도 : zzz
-            // 위도 : xxx , 경도 : zzz
-            // 위도 : xxx , 경도 : zzz
-            for(let i = 0; i < json2.length; i++){
-               result[i]= [`위도 : ${json2[i].lat}`, `경도 : ${json2[i].lng}`];
-            }
-            return result;
-        }
-        
-        const json = '[{"lat":34.123, "lng":127.123}, {"lat":35.123, "lng":132.345}, {"lat":36.123, "lng":136.456}]'
-        console.log(latLng(json));
-    }
-    {   //case2)
-        function latLng(json){
-            // 1) json포맷 문자열 => js로 변환
-            const json2 = JSON.parse(json);
-            let result = [];
-            let a = [];
-            let b = [];
-            // 2) 
-            // 위도 : xxx , 경도 : zzz
-            // 위도 : xxx , 경도 : zzz
-            // 위도 : xxx , 경도 : zzz
-            for(let i = 0; i < json2.length; i++){
-               a[i] = json2[i].lat;
-               b[i] = json2[i].lng;
-            }
-            result.push(`위도: ${a}`);
-            result.push(`경도: ${b}`);
-            return result;
-        }
-        
-        const json = '[{"lat":34.123, "lng":127.123}, {"lat":35.123, "lng":132.345}, {"lat":36.123, "lng":136.456}]'
-        console.log(latLng(json));
-    }
-    
-    
 }
--- 7. 문제/problem5_sol.js
+++ 7. 문제/problem5_sol.js
@@ -1,120 +1,175 @@
+const data = `{
+    "members":[
+      {
+        "id":1,
+        "name":"홍길동1",
+        "age":10,
+        "gender":"남",
+        "hobby" : ["수영","등산","골프"]
+      },
+      {
+        "id":2,
+        "name":"홍길동2",
+        "age":20,
+        "gender":"여",
+        "hobby" : ["독서","수영"]
+      },
+      {
+        "id":3,
+        "name":"홍길동3",
+        "age":30,
+        "gender":"남",
+        "hobby" : ["영화","축구","게임"]
+      },
+      {
+        "id":4,
+        "name":"홍길동4",
+        "age":40,
+        "gender":"여",
+        "hobby" : ["수영","등산"]
+      },
+      {
+        "id":5,
+        "name":"홍길동5",
+        "age":50,
+        "gender":"여",
+        "hobby" : ["음악","영화","골프"]
+      }
+    ]
+  }`;
+
+// 공공API를 통해 위의 JSON포멧의 문자열을 받았다고 가정하고 아래문항에 대해 각각 코드로 구현하시오
+const result = JSON.parse(data);
+
+// 1. 회원수는?
 {
-    // 문제1) 정수를 요소로 갖는 배열을 매개값으로 받아 합과 평균을 반환하는 함수를 구현하시오
-    
-{       //case1) 객체리터럴 반환
-    function sumAndAvg(arr){
-        const result = {};
-
-        let sum = 0;
-        for(let i = 0; i < arr.length; i++){
-            sum += arr[i];
-        }
-        const avg = sum / arr.length;
-        result.sum = sum;   //{ 'sum': sum }
-        result.avg = avg;   //{ 'sum': sum, 'avg': avg }
-    
-        return result;
-    }
-    const values = [1,2,3];
-    const result = sumAndAvg(values);
-    console.log(`합: ${result['sum']}, 평균: ${result['avg']}`);
+    console.log(`회원수: ${result.members.length}`);
 }
-
-{       //case2) 배열 
-    function sumAndAvg(arr){
-        const result = [];
-
-        let sum = 0;
-        for(let i = 0; i < arr.length; i++){
-            sum += arr[i];
-        }
-        const avg = sum / arr.length;
-
-        result.push(sum);   //[sum]
-        result.push(avg);   //[sum,avg]
-
-        return result;
-    }
-    const values = [1,2,3];
-    const result = sumAndAvg(values);
-    console.log(`합: ${result[0]}, 평균: ${result[1]}`);
-
-}
-
-{       //case3) set
-    function sumAndAvg(arr){
-        const result = new Set();
-
-        let sum = 0;
-        for(let i = 0; i < arr.length; i++){
-            sum += arr[i];
-        }
-        const avg = sum / arr.length;
-
-        result.add(sum);   //{[sum]}
-        result.add(avg);   //{[sum,avg]}
-
-        return result;
-    }
-    const values = [1,2,3];
-    const result = sumAndAvg(values);
-    
-    result.forEach(function(val){
-        console.log(val);
-    });
-}
-
-{       //case4) map
-    function sumAndAvg(arr){
-        const result = new Map();
-
-        let sum = 0;
-        for(let i = 0; i < arr.length; i++){
-            sum += arr[i];
-        }
-        const avg = sum / arr.length;
-
-        result.set('sum',sum);   //{ 'sum' => 6 }
-        result.set('avg',avg);   //{ 'sum' => 6, 'avg' => 2 }
-        return result;
-    }
-    const values = [1,2,3];
-    const result = sumAndAvg(values);
-    console.log(`합: ${result.get('sum')}, 평균: ${result.get('avg')}`);
-    // console.log(result);
-}
-
-    // 문제2) 구구단 단수를 매개값으로 받아 구구단을 출력하는 함수를 구현하시오
+// 2. 여성회원수는?
 {
-    function gugudan(dansu){
-        console.log(`${dansu}단 출력`);
-        for(let i = 1; i <= 9; i++){
-            // 2 * 1 = 2 ..
-            console.log(`${dansu} * ${i} = ${dansu*i}`);
-        }
-        // return undefined;
+  { //case1)
+    let count = 0;
+    for(let i = 0; i < result.members.length; i++){
+      if(result.members[i].gender == '여') {
+        count++
+      }
     }
-    const result = gugudan(3);
-    // console.log(result);
-}   
-
-    // 문제3) 아래 해당되는 문자열을 매개값으로 받아 위도(lat)와 경도(lng)를 출력하는 함수를 구현하시오
-{
-    function latLng(json){
-        // 1) json포맷 문자열 => js로 변환
-        const arr = JSON.parse(json);
-        
-        // 2) 
-        for(let i = 0; i < arr.length; i++){
-            console.log(`위도: ${arr[i].lat}, 경도: ${arr[i].lng}`);
-        }
-        // 위도 : xxx , 경도 : zzz
-        // 위도 : xxx , 경도 : zzz
-        // 위도 : xxx , 경도 : zzz
-    }
-    
-    const json = '[{"lat":34.123, "lng":127.123}, {"lat":35.123, "lng":132.345}, {"lat":36.123, "lng":136.456}]'
-    latLng(json);
+    console.log(`여자회원 수 = ${count}`);
+  }
+  { //case2)
+    console.log(`여자회원 수 = ${result.members.filter(ele => ele.gender == '여').length}`);
+  }
 }
-    
+// 3. 남자회원 나이의 총합과 여자회원 나이의 총합?
+{
+  { //case1) 반복문
+    let ageOfwomen = 0;
+    let ageOfmen = 0;
+    for(let i = 0; i < result.members.length; i++){
+      if(result.members[i].gender == '여') {
+        ageOfwomen += result.members[i].age;
+      }else if(result.members[i].gender == '남'){
+        ageOfmen += result.members[i].age;
+      }
+
+    }
+    console.log(`여자회원 나이 총합 = ${ageOfwomen}`);
+    console.log(`남자회원 나이 총합 = ${ageOfmen}`);
+  }
+  { //case2) 고차함수
+    const ageOfWomen = result.members.filter(ele => ele.gender == '여')
+                                     .reduce((acc,ele) => acc + ele.age, 0);
+    const ageOfmen = result.members.filter(ele => ele.gender == '남')
+                                     .reduce((acc,ele) => acc + ele.age, 0);
+
+    console.log(`여자회원 나이 총합 = ${ageOfWomen}`);
+    console.log(`남자회원 나이 총합 = ${ageOfmen}`);
+  }
+}
+// 4. 골프가 취미인 회원의 이름을 나열하시오
+{
+    { //case1)
+        for(let i = 0; i < result.members.length; i++){
+        if(result.members[i].hobby.includes('골프')){
+          console.log(result.members[i].name);
+        }
+      }
+    }
+    { //case2)
+      result.members.forEach(ele => {
+        if(ele.hobby.includes('골프')){
+          console.log(ele.name);
+        }
+      });
+    }
+}
+// 5. 회원의 취미를 모두 나열하시오.
+{
+  { //case1) map, set, flat
+    const hobbys = result.members.map(member => member.hobby);
+    console.log(hobbys);
+    console.log(hobbys.flat());
+    console.log(new Set(hobbys.flat()));
+  }
+  
+  { //case2)mep, set, flat, forEach
+    const hobbys2 = new Set(result.members.map(member => member.hobby).flat());
+    hobbys2.forEach(ele => console.log(ele));
+    hobbys2.forEach(console.log);
+  }
+
+  { //case3)mep, reduce, forEach, set
+    const hobbys3 =  result.members.map(m => m.hobby)
+                                .reduce((acc, hobby) => acc.concat(hobby),[]);
+    (new Set(hobbys3)).forEach(hobby => console.log(hobby));
+  }
+}
+
+// 6. 여성회원중 나이가 가장 많은 회원의 이름은?
+{
+  { //case1)
+    // 1)여성회원 추출
+    const women = [];
+    for(let i = 0; i < result.members.length; i++){
+      if(result.members[i].gender == '여'){
+        women.push(result.members[i]);
+      }
+    }
+    // 2)여성회원 중 나이가 가장 많은 회원의 이름
+      let maxAgeOfWomen = women[0].age;
+      let idxOfMaxAgeOfWomen = 0;
+      for(let i = 0; i < women.length; i++){
+        if(women[i].age > maxAgeOfWomen){
+          maxAgeOfWomen = women[i].age;
+          idxOfMaxAgeOfWomen = i;
+        }
+      }
+
+      console.log(`이름 = ${women[idxOfMaxAgeOfWomen].name}`);
+    }
+
+    { //case2) 고차함수 filter, sort
+      console.log(result.members.filter( m => m.gender == '여' )
+                                .sort((m1,m2) => (m2.age - m1.age))[0].name
+      );
+    }
+  }
+
+
+// 7. 모든 회원이 여성회원들로만 이뤄져있는지 판단하시오
+{
+  const isWomen = result.members.every(member => member.gender == '여');
+  if(isWomen){
+    console.log('모든 회원이 여성이다');
+  }else{
+    console.log('남성회원이 존재한다');
+  }
+}
+{ // 중괄호 있으면 return문이 들어가야함
+  const existWomen = result.members.some(member => {return member.gender == '여'}); 
+  if(existWomen){
+    console.log('여성회원이 있습니다');
+  }else{
+    console.log('여성회원은 없습니다');
+  }
 }
