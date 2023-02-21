1979fe7f157223e2b2aa40c92532aff0aac23ae4
NanGyung <rhrnakaktdidrod0926@gmail.com>
2023-02-21 15:29:07 +0900

    23.02.21

---
+++ 7. 문제/reduce.js
@@ -0,0 +1,56 @@
+// 문제1) 배열의 모든 요소를 모두 합산하는 코드를 구현하시오(Array.prototype.reduce()사용)
+{
+    const arr = Array.of(1,2,3,4,5);
+
+    const f = (acc,curr) => acc+curr;
+    const result  = arr.reduce(f,0);
+    console.log(`합계 = ${result}`);
+
+}
+// 문제2) 배열읨 모든 요소 중 최대값을 구하시오
+{
+    const arr = Array.of(3,2,5,4,1);
+
+    /* const result = arr.reduce((acc,curr)=>{
+        // 1. acc = 0, curr = 3
+        // 2. acc = 3, curr = 2
+        // 3. acc = 3, curr = 5
+        // 4. acc = 5, curr = 4
+        // 5. acc = 5, curr = 1
+        // 최종 = 5
+        return acc > curr ? acc : curr;},0); */
+        
+    const max = arr.reduce((acc,curr) => acc > curr ? acc : curr, 0);
+    const min = arr.reduce((acc,curr) => acc < curr ? acc : curr);
+    console.log(`최대값 = ${max}`);
+    console.log(`최소값 = ${min}`);
+}
+// 문제3) 배열의 중복을 제거하시오
+{
+    const arr = [1,2,1,3,5,4,5,3,4,4];
+                // [1,2,3,5,4]
+    const f = function(){
+        
+    }
+
+        // 1. acc = [] curr = 1         
+        // 2. acc = [1] curr = 2
+        // 3. acc = [1,2] curr = 1
+        // 4. acc = [1,2] curr = 3
+        // 5. acc = [1,2,3] curr = 5
+        // 6. acc = [1,2,3,5] curr = 4
+        // 7. acc = [1,2,3,5] curr = 5
+        // acc 배열 요소에 curr값이 없으면 acc에 추가
+
+    const result = arr.reduce((acc,curr)=>{
+        // case 1)
+        // if(!acc.includes(curr)){
+        //      acc.push(curr);
+        // } 
+        
+        // case 2)
+        if(acc.indexOf(curr)== -1) acc.push(curr);
+        return acc;
+    },[]);
+    console.log(result.sort());    //[1,2,3,4,5]
+}
